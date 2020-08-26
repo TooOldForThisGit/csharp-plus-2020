dfb7d60ca7560f9f7296babe424195047eab914a
TooOldForThisGit <j.o.andersson@hotmail.se>
2020-08-26 11:49:10 +0200

    Nameinput commit.

---
+++ Session02/Exercise01/Nameprogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Nameprogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to enter your name? (y/n)");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'n')
+                return;
+
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+
+
+        }
+    }
+}
