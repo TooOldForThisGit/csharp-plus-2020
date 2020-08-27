a27e590c750ff0bed3acdef8d58a7665b119266e
TooOldForThisGit <j.o.andersson@hotmail.se>
2020-08-27 11:41:31 +0200

    String test

--- Session2/Session02/Session02Exercise01/Program.cs
+++ Session2/Session02/Session02Exercise01/Program.cs
@@ -7,6 +7,10 @@ namespace Session02Exercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string stringValue = "Mystringvalue";
+
+            Console.WriteLine("value of my stringvalue: " + stringValue  );
         }
     }
 }
