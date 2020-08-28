01907aedf3f99aa0fb89075c1cc8035675b44312
TooOldForThisGit <j.o.andersson@hotmail.se>
2020-08-28 13:09:23 +0200

    Progress on number app

--- Session03/Session03Exercise02/Program.cs
+++ Session03/Session03Exercise02/Program.cs
@@ -1,5 +1,8 @@
 ﻿        
 using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 
 namespace Session03Exercise02
 {
@@ -17,6 +20,45 @@ namespace Session03Exercise02
             }
 
             Console.ReadKey();
+            
+            Console.WriteLine("Ange ett antal siffror, separat med kommatecken.");
+            var inputNumber = Console.ReadLine();
+
+            var inputStringArray = inputNumber.Split(',', StringSplitOptions.RemoveEmptyEntries);//new [] för FrameWork 
+            var numbers = new double[inputStringArray.Length];
+            double middlevalue = 0;
+
+            //while()
+            for (int i = 0; i < inputStringArray.Length; i++)
+            {
+                Console.WriteLine("Värdet är:".PadRight(14) + inputStringArray[i]);
+                try
+                {
+                    double currentNumber = 0;
+                    NumberStyles numberStyles = NumberStyles.Any | NumberStyles.Float; //bitwiseoperator
+                    bool parsed  = Double.TryParse(inputStringArray[i], numberStyles, CultureInfo.InvariantCulture, out currentNumber);
+                    if (parsed)
+                    {
+                        numbers[i] = currentNumber;
+                        middlevalue += currentNumber;
+                    }
+                    else continue;
+                    
+                    // Debug.Assert(i < numbers.Length - 1);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+            }
+            Console.WriteLine("-------------------------------------------\n");
+            Console.WriteLine($"Högsta värdet: {numbers.Max()}");//Max value  
+            Console.WriteLine($"Minsta värdet: {numbers.Min()}");//Min value
+            Console.WriteLine("Medelvärdet för arrayen är: " + string.Format($"{(middlevalue / numbers.Length):0.00}")); //Middle value
+            Console.ReadKey();
+
         }
     }
 }
